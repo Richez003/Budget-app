07c0f9017a5f4dfb44e74aa0a72ad618f9356b03
richez003 <richemonday@gmail.com>
2022-10-06 16:24:26 -0500

    created relationship and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :categories, dependent: :destroy
+  has_many :records, dependent: :destroy
+  validates :name, presence: true, length: { minimum: 1, maximum: 150 }
 end

03869e19021840568466cdba90d433290a72bd28
richez003 <richemonday@gmail.com>
2022-10-06 17:29:13 -0500

    created user model test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,21 +1,22 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-# RSpec.describe Group, type: :model do
+# RSpec.describe Category, type: :model do
 #   before :each do
 #     @user = User.new(name: 'test', email: 'test@gmail.com')
-#     @category = category.new(name: 'group', icon: 'group icon', user: @user)
+#     @category = category.new(name: 'category', icon: 'category icon', user: @user)
 #   end
+# end
 
-#   it 'shows exact name' do
-#     expect(@category.name).to eql('category')
-#     expect(@category.icon).to eql('category icon')
-#   end
-  # it 'can not create a group without  a name' do
-  #   group = Group.create(icon: 'group icon', name: nil, user: @user.id)
-  #   expect(group).to_not be_valid
-  # end
-  # it 'can not create a group without  an icon' do
-  #   group = Group.create(name: 'group ', icon: nil, user: @user.id)
-  #   expect(group).to_not be_valid
-  # end
-end
+# #   it 'shows exact name' do
+# #     expect(@category.name).to eql('category')
+# #     expect(@category.icon).to eql('category icon')
+# #   end
+#   # it 'can not create a group without  a name' do
+#   #   group = Group.create(icon: 'group icon', name: nil, user: @user.id)
+#   #   expect(group).to_not be_valid
+#   # end
+#   # it 'can not create a group without  an icon' do
+#   #   group = Group.create(name: 'group ', icon: nil, user: @user.id)
+#   #   expect(group).to_not be_valid
+#   # end
+# # end

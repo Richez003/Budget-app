abec08581d92554a973e9a72434eaf2c02647f5c
richez003 <richemonday@gmail.com>
2022-10-06 16:36:24 -0500

    installed rspec

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,6 @@
 class Record < ApplicationRecord
   belongs_to :user
-  has_many :category,
+  has_many :category
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }

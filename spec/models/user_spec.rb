c3c8638635156ede5635519b424ce8c8e39cfe71
richez003 <richemonday@gmail.com>
2022-10-06 16:58:56 -0500

    geneated devise user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+# describe User, type: :model do
+  # it 'should be invalid without an email' do
+  #   user = User.create(name: 'Test', email: nil, password: 123456, password_confirmation: 123456)
+  #   expect(user).to_not be_valid
+  # end
+#   it 'should be valid' do
+#     user = User.create(name: 'Test')
+#     expect(user).to be_valid
+#   end
+# end

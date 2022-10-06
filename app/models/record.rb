71a382328209d0b78147105addfb0655464fc9a3
richez003 <richemonday@gmail.com>
2022-10-06 16:25:36 -0500

    created record relationship and validation

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,7 @@
 class Record < ApplicationRecord
+  belongs_to :user
+  has_many :category,
 
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end

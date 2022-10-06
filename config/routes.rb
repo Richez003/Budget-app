df861b0e876d70d7720ff4dc0ca4f819ee5aa377
richez003 <richemonday@gmail.com>
2022-10-06 12:37:34 -0500

    updated users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  get 'users/show'
+  # get 'users/show'
   devise_for :users
+  root  "users#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

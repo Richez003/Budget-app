b40990646817b6da270711aa65bcb2c1db4aac5e
richez003 <richemonday@gmail.com>
2022-10-06 19:12:47 -0500

    created method for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,26 @@
 class CategoriesController < ApplicationController
   def index
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @user = current_user
+    @category = @user.categories.new(category_params)
+    if @@category.save
+    flash[:notice] = "#{@category.name} created successfully!"
+    redirect_to category_index_path
+  else
+    flash[:alert] = @category.errors.full_messages.first
+    render :new
+  end
+end
+
+private
+
+def category_params
+  params.require(:category).permit(:name, :icon)
+end
 end
